be8ebc1394172b16b2622a2d37f9d5907ada4edd
micihime <hroncovam@yahoo.com>
2021-03-16 19:24:48 +0100

    Uloha 1

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // tady je místo pro náš program
+
+function makeBoldOnMouseOver() {
+    console.log("in mouse over");
+    //document.querySelector("#myDiv").style.fontWeight = "900";
+    document.querySelector("#myDivv").classList.toggle("div-bold");
+}
+
+function makeBold() {
+    console.log("in mouse enter");
+    document.querySelector("#myDiv").classList.add("div-bold");
+}
+
+function makeThin() {
+    console.log("in mouse out");
+    document.querySelector("#myDiv").classList.remove("div-bold");
+}
